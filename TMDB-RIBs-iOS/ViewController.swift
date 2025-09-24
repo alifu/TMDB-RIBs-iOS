373f3504a8302b44c5726e41e366b02f3c9b37e7
alifu <alif.bangets@gmail.com>
2025-09-24 16:31:01 +0700

    initial commit

---
+++ TMDB-RIBs-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB-RIBs-iOS
+//
+//  Created by Alif on 18/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TMDB-RIBs-iOSUITests/TMDB_RIBs_iOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TMDB_RIBs_iOSUITestsLaunchTests.swift
+//  TMDB-RIBs-iOSUITests
+//
+//  Created by Alif on 18/09/25.
+//
+
+import XCTest
+
+final class TMDB_RIBs_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
